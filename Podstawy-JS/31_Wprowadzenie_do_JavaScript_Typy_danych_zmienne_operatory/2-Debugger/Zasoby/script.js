88d45784a495fcdb878b84d27bd6a20d41e5c3cb
kasperdob <kasperdob@gmail.com>
2025-10-16 15:46:03 +0200

    Dodanie lekcji instrukcje if oraz petle

--- Podstawy-JS/31_Wprowadzenie_do_JavaScript_Typy_danych_zmienne_operatory/2-Debugger/Zasoby/script.js
+++ Podstawy-JS/31_Wprowadzenie_do_JavaScript_Typy_danych_zmienne_operatory/2-Debugger/Zasoby/script.js
@@ -4,15 +4,38 @@ function sumujLosoweLiczby() {
     let b = Math.floor(Math.random() * 10);
     let suma = a + b;
 
+    const p = 7;
+
+    var x = 5;
+    let y = 5;
+
+    if (x == y) {
+      console.log("==");  
+    }
+    if (x === y) {
+      console.log("===");  
+    }
+
     console.log("Liczba a:", a);
     console.log("Liczba b:", b);
     console.log("Suma:", suma);
-
+    //ggg
     return suma;
 }
 
+function zwrocImie() {
+    let name = document.getElementById("nameInput").value;
+    return name;
+}
+
 // Obsługa kliknięcia przycisku
+// document.getElementById("startBtn").addEventListener("click", () => {
+//     let wynik = sumujLosoweLiczby();
+//     alert("Wynik sumy to: " + wynik);
+
+// });
+
 document.getElementById("startBtn").addEventListener("click", () => {
-    let wynik = sumujLosoweLiczby();
-    alert("Wynik sumy to: " + wynik);
-});
+    alert(sumujLosoweLiczby());
+    console.log(x);
+})
